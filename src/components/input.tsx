13e3e5bfd071901bc022c775ea99335ef37f9783
k-sang-soo <wsckm1@google.com>
2023-01-31 22:44:09 +0900

    데이터베이스 설정 및 연결, enter form 작업 중...

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,20 +1,36 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+
 interface InputProps {
     label: string;
     name: string;
     kind?: 'text' | 'phone' | 'price';
-    [key: string]: any;
+    register: UseFormRegisterReturn;
+    type: string;
+    required: boolean;
 }
-export default function Input({ label, name, kind = 'text', ...rest }: InputProps) {
+export default function Input({
+    label,
+    name,
+    kind = 'text',
+    register,
+    type,
+    required,
+}: InputProps) {
     return (
         <div>
-            <label className="mb-1 block text-sm font-medium text-gray-700" htmlFor={name}>
+            <label
+                className="mb-1 block text-sm font-medium text-gray-700"
+                htmlFor={name}
+            >
                 {label}
             </label>
             {kind === 'text' ? (
                 <div className="rounded-md relative flex  items-center shadow-sm">
                     <input
                         id={name}
-                        {...rest}
+                        {...register}
+                        type={type}
+                        required={required}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
@@ -26,7 +42,9 @@ export default function Input({ label, name, kind = 'text', ...rest }: InputProp
                     </div>
                     <input
                         id={name}
-                        {...rest}
+                        {...register}
+                        type={type}
+                        required={required}
                         className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                     <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -36,10 +54,14 @@ export default function Input({ label, name, kind = 'text', ...rest }: InputProp
             ) : null}
             {kind === 'phone' ? (
                 <div className="flex rounded-md shadow-sm">
-                    <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">+82</span>
+                    <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
+                        +82
+                    </span>
                     <input
                         id={name}
-                        {...rest}
+                        {...register}
+                        type={type}
+                        required={required}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
