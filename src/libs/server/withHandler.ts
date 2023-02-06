f7937489289dd5c5a0a106787806c97bba26eb2f
k-sang-soo <wsckm1@google.com>
2023-02-06 22:20:58 +0900

    email, phone db 연결, token 제작 중...

--- src/libs/server/withHandler.ts
+++ src/libs/server/withHandler.ts
@@ -1,9 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+//고차함수
 export default function withHandler(
     method: 'GET' | 'POST' | 'DELETE',
     fn: (req: NextApiRequest, res: NextApiResponse) => void,
 ) {
     return async function (req: NextApiRequest, res: NextApiResponse) {
-        res.json({ hello: true });
+        if (req.method !== method) {
+            return res.status(405).end();
+        }
+        try {
+            await fn(req, res);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({ error });
+        }
     };
 }
--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -3,8 +3,57 @@ import withHandler from '@/libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.body);
-    res.status(200).end();
+    const { phone, email } = req.body;
+    const payload = phone ? { phone: +phone } : { email };
+    const user = await client.user.upsert({
+        where: {
+            ...payload,
+        },
+        create: {
+            name: 'Anonymous',
+            ...payload,
+        },
+        update: {},
+    });
+
+    /*     if (email) {
+        user = await client.user.findUnique({
+            where: {
+                email,
+            },
+        });
+        if (user) console.log('찾음');
+        if (!user) {
+            console.log('못 찾음 새로 만들겠음');
+            user = await client.user.create({
+                data: {
+                    name: 'Anonymous',
+                    email,
+                },
+            });
+        }
+        console.log(user);
+    }
+    if (phone) {
+        user = await client.user.findUnique({
+            where: {
+                phone: +phone,
+            },
+        });
+        if (user) console.log('찾음');
+        if (!user) {
+            console.log('못 찾음 새로 만들겠음');
+            user = await client.user.create({
+                data: {
+                    name: 'Anonymous',
+                    phone: +phone,
+                },
+            });
+        }
+        console.log(user);
+    } */
+
+    return res.status(200).end();
 }
 
 export default withHandler('POST', handler);
