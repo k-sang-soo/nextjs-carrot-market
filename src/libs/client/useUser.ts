2045893929eb398c40a854f1f694795e9791ca84
k-sang-soo <wsckm1@google.com>
2023-02-20 22:30:18 +0900

    SWR 일부 적용

--- src/libs/client/useUser.ts
+++ src/libs/client/useUser.ts
@@ -1,18 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-    const [user, setUser] = useState();
+    const { data, error } = useSWR('/api/users/me', fetcher);
     const router = useRouter();
-    useEffect(() => {
-        fetch('/api/users/me')
-            .then((response) => response.json())
-            .then((data) => {
-                if (!data.ok) {
-                    router.replace('/enter');
-                }
-                setUser(data.profile);
-            });
-    }, [router]);
-    return user;
+
+    //router.replace('/enter');
+    return data;
 }
